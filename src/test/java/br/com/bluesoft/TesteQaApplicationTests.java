6436fdf1c21852668cfedaf2b13e2c8ca92cecbe
amandaessilva <amanda.elensantos@gmail.com>
2023-04-12 11:05:42 -0300

    Ajustes em comentários

--- src/test/java/br/com/bluesoft/TesteQaApplicationTests.java
+++ src/test/java/br/com/bluesoft/TesteQaApplicationTests.java
@@ -1,11 +1,7 @@
 package br.com.bluesoft;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,20 +14,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class TesteQaApplicationTests {
 	private static WebDriver chromeDriver;
-
+// Metodo para criar uma unica instancia do chrome driver e assim abrir uma unica janela do navegador para executar todos os testes
 	@BeforeClass
 	public static void setup(){
 		WebDriverManager.chromedriver().setup();
 		chromeDriver = new ChromeDriver();
 		chromeDriver.manage().window().maximize();
 	}
+	// Metodo que limpa a base antes de executar cada teste
+	@Before
+	public void LimparDados(){
+		chromeDriver.get("http://localhost:8080");
+		chromeDriver.findElement(By.className("btn-danger")).click();
 
-	//@Test
-	// public void contextLoads() {}
-
-	// Teste de busca de campos no chrome ok
+	}
+	// Teste de busca de campos
  	@Test
-	public void findFields(){
+	public void FindFields(){
 		chromeDriver.get("http://localhost:8080");
 		chromeDriver.findElement(By.id("nome"));
 		chromeDriver.findElement(By.id("cpf"));
@@ -43,48 +42,51 @@ public class TesteQaApplicationTests {
 		chromeDriver.findElement(By.cssSelector(".btn-danger"));
 	}
 
-	//teste de cadastro de usuarios no chrome
+	// Teste de cadastro de usuarios
 
 	@Test
-	public void createMultipleUsers(){
+	public void CreateMultipleUsers(){
 		createUser("Marcos Vinicius Emanuel Thomas Galvão", "462.258.965-63", "(17)99676-2672", "28/02/1994");
 		createUser("André Luan Fernando Pereira", "452.301.249-03", "(43) 98328-6989", "07/04/1999");
 		createUser("Tânia Larissa Peixoto", "638.627.249-80", "(43) 2833-0160", "28/02/1994");
 		createUser("Kauê Jorge Benjamin Almada", "055.095.222-59", "(92) 99220-1050", "24/02/1999");
-		WebDriverWait espera = new WebDriverWait(chromeDriver, 20);
+
+		WebElement element = chromeDriver.findElement(By.id("tabela-usuarios"));
+		WebDriverWait wait = new WebDriverWait(chromeDriver, 5);
+		wait.until(ExpectedConditions.textToBePresentInElement(element, "André Luan Fernando Pereira"));
 	}
-	////teste de busca de usuarios no chrome ok
+	// Teste de busca de usuarios
 	@Test
-	public void findUser(){
+	public void FindUser(){
 		createUser("André Luan Fernando Pereira", "452.301.249-03", "(43) 98328-6989", "07/04/1999");
 
-		WebElement elemento = chromeDriver.findElement(By.id("tabela-usuarios"));
+		WebElement element = chromeDriver.findElement(By.id("tabela-usuarios"));
 
-		WebDriverWait espera = new WebDriverWait(chromeDriver, 5);
-		espera.until(ExpectedConditions.textToBePresentInElement(elemento, "André Luan Fernando Pereira"));
+		WebDriverWait wait = new WebDriverWait(chromeDriver, 5);
+		wait.until(ExpectedConditions.textToBePresentInElement(element, "André Luan Fernando Pereira"));
 	}
-	//teste de exclusão de usuarios no chrome  ok
+	// Teste de exclusão de usuarios
 	@Test
-	public void deleteUsers(){
-		createMultipleUsers();
+	public void DeleteUsers(){
+		CreateMultipleUsers();
 		chromeDriver.findElement(By.cssSelector(".btn-danger")).click();
 	}
-
+	// Teste de formato invalido de data
 	@Test
-	public void validateBirthdateFormat(){
+	public void ValidateBirthdateFormat(){
 		createUser("Marcos Vinicius Emanuel Thomas Galvão", "462.258.965-63", "(17)99676-2672", "28021994");
 
-		WebElement elemento = chromeDriver.findElement(By.id("tabela-usuarios"));
+		WebElement element = chromeDriver.findElement(By.id("tabela-usuarios"));
 
 		try {
-			WebDriverWait espera = new WebDriverWait(chromeDriver, 2);
-			espera.until(ExpectedConditions.textToBePresentInElement(elemento, "Marcos Vinicius Emanuel Thomas Galvão"));
+			WebDriverWait wait = new WebDriverWait(chromeDriver, 2);
+			wait.until(ExpectedConditions.textToBePresentInElement(element, "Marcos Vinicius Emanuel Thomas Galvão"));
 			Assert.fail("A exceção .TimeoutException não foi lançada");
 		} catch (org.openqa.selenium.TimeoutException e){
 			System.out.println("Exception lançada: "+ e);
 		}
 	}
-
+	// Método que recebe como parametros os dados do usuário e o cadastra
 	private void createUser(String name, String cpf, String phone, String birthdate){
 		chromeDriver.get("http://localhost:8080");
 	 	chromeDriver.findElement(By.id("nome")).sendKeys(name);
@@ -93,9 +95,9 @@ public class TesteQaApplicationTests {
 		chromeDriver.findElement(By.id("dt-nascimento")).sendKeys(birthdate);
 		chromeDriver.findElement(By.id("btn-salvar")).click();
 	}
-
+	// TearDown
 	@AfterClass
-	public static void tearDown() {
+	public static void TearDown() {
 		chromeDriver.quit();
 	}
 }
