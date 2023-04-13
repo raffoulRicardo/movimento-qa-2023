21f61de901cdd4e1d23ca6349e1b20bd827de7f3
Odair <opanciera@hotamil.com>
2023-04-12 21:48:34 -0300

    preparado o ambiente par começar a construir conexão e os metodos para os testes
    feito o primeiro @Test com a conexão, feito tambem os primeiros metodos

--- src/test/java/page/CadastroPage.java
+++ src/test/java/page/CadastroPage.java
@@ -1,4 +1,35 @@
 package page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CadastroPage {
+    public static final String URL_8080 = "http://localhost:8080/?";
+    public WebDriver browser;
+
+    public CadastroPage() {
+        System.setProperty("web-driver.chrome.driver", "/path/to/chromedriver");
+        this.browser = new ChromeDriver();
+        browser.navigate().to(URL_8080);
+    }
+    public void fechar() {
+        this.browser.quit();
+    }
+
+    public void preencheFormulario(String nome, String cpf, String celular, String dtNascimento) {
+
+        this.browser.findElement(By.id("nome")).clear();
+        this.browser.findElement(By.id("nome")).sendKeys(nome);
+        this.browser.findElement(By.id("cpf")).clear();
+        this.browser.findElement(By.id("cpf")).sendKeys(cpf);
+        this.browser.findElement(By.id("celular")).clear();
+        this.browser.findElement(By.id("celular")).sendKeys(celular);
+        this.browser.findElement(By.id("dt-nascimento")).clear();
+        this.browser.findElement(By.id("dt-nascimento")).sendKeys(dtNascimento);
+    }
+    public void clicaSalvarFormulario() {
+        this.browser.findElement(By.id("btn-salvar")).click();
+    }
+
 }
--- src/test/java/tests/CadastroTest.java
+++ src/test/java/tests/CadastroTest.java
@@ -1,4 +1,38 @@
 package tests;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import page.CadastroPage;
+
 public class CadastroTest {
+    private WebDriver browser;
+
+    private CadastroPage paginaDeCadastro;
+    @BeforeEach // sempre que iniciar vai passar por aqui primeiro
+    public void iniciaAqui(){
+        this.paginaDeCadastro = new CadastroPage();
+    }
+    @AfterEach
+    public void finalizaAqui(){
+        this.paginaDeCadastro.fechar();
+    }
+    @Test
+    public void preencherCamposDadosValidosESalvar(){
+        paginaDeCadastro.preencheFormulario("Pirulito Rex", "628.252.240-49", "(84) 99634-6633", "11/04/1954");
+        paginaDeCadastro.clicaSalvarFormulario();
+
+//        Assert.assertTrue(paginaDeCadastro.validarCampoNome("Pirulito Rex"));
+//        Assert.assertTrue(paginaDeCadastro.validarCampoCpf("628.252.240-49"));
+//        Assert.assertTrue(paginaDeCadastro.validarCampoCelular("(84) 99634-6633"));
+//        Assert.assertTrue(paginaDeCadastro.validarCampoDtNascimento("11/04/1954"));
+
+        paginaDeCadastro.fechar();
+    }
+//    @Test
+//    public void BuscarPorNome(){
+//        paginaDeCadastro.buscaPorNome("Tio do Sorvete");
+//    }
 }
