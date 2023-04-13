2ff263b6835bc7b3cdfe0df979c59cfcd11be7b6
Sarah Ishida <82983033+sarahishida@users.noreply.github.com>
2023-04-13 07:41:47 -0300

    automacao usando o selenium ide

--- src/test/java/br/com/bluesoft/TesteQaApplicationTests.java
+++ src/test/java/br/com/bluesoft/TesteQaApplicationTests.java
@@ -7,7 +7,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TesteQaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testeSeleniumIDE() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+		WebDriver driver = new ChromeDriver(capabilities);
+
+		driver.get("file:///{/home/sarah/movimento-qa-2023/movimento-qa-2023/src/test/selenium/Automacao-Sarah-Ishida.side}");
+		driver.quit();
 	}
 
 }
